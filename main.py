78c15aed9ffc3e5772c9e9be6d36e3d3c2ebac60
MediJaster <caddeogiorgio1@gmail.com>
2022-11-09 00:08:51 +0100

    support for multiple playlists
    The program now cycles through a list of playlist IDs and saves everything to one file. Fixed files not being closed after each use

--- main.py
+++ main.py
@@ -24,51 +24,64 @@ def get_playlist_items(playlist_id, page_id=None):
 
     return request.execute()
 
-with open("saved_playlists.json", "r") as json_file:
-    saved_playlists = json.load(json_file)
+saved_playlists = {}
+
+try:
+    with open("saved_playlists.json", "r") as json_file:
+        saved_playlists = json.load(json_file)
+        json_file.close()
+
+except FileNotFoundError:
+    json_file = open("saved_playlists.json", "w")
+    json.dump({}, json_file)
+    json_file.close()
 
 def main():
-    playlist_id = "PLCvPrFTfR1h4ZRcYgRSEdo7Rx9fq5GzMI"
-    page_id = ""
-    items = []
+    playlist_ids = ["PLCvPrFTfR1h4ZRcYgRSEdo7Rx9fq5GzMI", "PLCvPrFTfR1h6ARHTeh6AeWrChhPSad3-w"]
+
+    for playlist_id in playlist_ids:
+        
+        page_id = ""
+        items = []
+
+        while page_id is not None:
+            response = get_playlist_items(playlist_id, page_id)
+
+            for item in response["items"]:
+
+                title = item["snippet"]["title"]
+
+                if title != "Deleted video":
+
+                    id = item["contentDetails"]["videoId"]
+                    url = f"https://www.youtube.com/watch?v={id}"
+
+                    item_dict = {
+                        "id" : id,
+                        "url" : url,
+                        "title" : title
+                    }
+
+                    items.append(item_dict)
 
 
-    while page_id is not None:
-        response = get_playlist_items(playlist_id, page_id)
+            try:
+                page_id = response["nextPageToken"]
 
-        for item in response["items"]:
+            except KeyError:
+                page_id = None
+                break
+        
+        saved_playlists[playlist_id] = {
+            "item_count" : len(items),
+            "items" : []
+        }
+        saved_playlists[playlist_id]["items"] = items
 
-            title = item["snippet"]["title"]
-
-            if title != "Deleted video":
-
-                id = item["contentDetails"]["videoId"]
-                url = f"https://www.youtube.com/watch?v={id}"
-
-                item_dict = {
-                    "id" : id,
-                    "url" : url,
-                    "title" : title
-                }
-
-                items.append(item_dict)
-
-
-        try:
-            page_id = response["nextPageToken"]
-
-        except KeyError:
-            page_id = None
-            break
-    
-    saved_playlists[playlist_id] = {
-        "item_count" : len(items),
-        "items" : []
-    }
-    saved_playlists[playlist_id]["items"] = items
 
     with open("saved_playlists.json", "w") as json_file:
         json.dump(saved_playlists, json_file)
+        json_file.close()
 
 
 
